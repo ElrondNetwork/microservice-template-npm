334ba120829a1b3de1dde93f5394235ab649a5bc
Traian Anghel <tanghel@live.com>
2022-06-27 17:44:40 +0300

    support for overriding logger

--- src/common/api-config/nestjs.api.config.module.ts
+++ src/common/api-config/nestjs.api.config.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import configuration from "../../../config/configuration";
+import { LoggingModule } from "../logging/logging.module";
 import { NestjsApiConfigService } from "./nestjs.api.config.service";
 
 @Global()
@@ -9,6 +10,7 @@ import { NestjsApiConfigService } from "./nestjs.api.config.service";
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    LoggingModule,
   ],
   providers: [
     NestjsApiConfigService,
